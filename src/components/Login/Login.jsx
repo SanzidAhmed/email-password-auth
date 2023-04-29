ae0d44e8874f4c5cbb5e3d766dc5a245d718c544
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-04-29 13:52:26 +0600

    commit after eid

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
+const auth = getAuth(app)
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        setError('');
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            if(!loggedUser.emailVerified){
+                alert('your email is not verified')
+            }
+            setSuccess('user login successful');
+            setError('')
+        })
+        .catch(error => {
+            setError(error.message);
+        })
+    }
     return (
-        <div>
-            <h1>This is login page</h1>
+        <div className='w-50 mx-auto'>
+            <h1>please login</h1>
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input
+                        type="email"
+                        name='email'
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        required
+                    />
+                </div>
+                <div className="form-group mt-2">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name='password'
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        required
+                    />
+                </div>
+                <div className="form-check mt-2">
+                    <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="rememberMe"
+                    />
+                    <label className="form-check-label" htmlFor="rememberMe">
+                        Remember me
+                    </label>
+                </div>
+                <p><small>new to this website? please <Link to="/register">register</Link></small></p>
+                <p>{success}</p>
+                <p className='text-danger'>{error}</p>
+                <button type="submit" className="btn btn-primary mt-2">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification} from 'firebase/auth'
 import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app);
 const Register = () => {
-    const [error, serError] =  useState('')
+    const [error, setError] =  useState('');
+    const [success, setSuccess] = useState('')
     const handleSubmit = (event) =>{
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
+        setSuccess('')
         console.log(email, password);
+        if(!/(?=.*[A-Z])/.test(password)){
+            setError('give a uppercase character');
+            return;
+        }
+        else if(!/(?=.*\d)/.test(password)){
+            setError('give a one numeric character'); 
+            return;
+        }
         createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
             const loggedUser = result.user;
-            console.log(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccess('welcome to register')
+            emailVerification(loggedUser)
         })
         .catch(error => {
-            serError(error.message);
+            setError(error.message);
+            
+        })
+    }
+    const emailVerification = (loggedUser) => {
+        sendEmailVerification(loggedUser)
+        .then(result => {
+            console.log(result);
+            alert('please check your email for verification')
         })
     }
     const handleEmailChange = (event) => {
@@ -32,7 +54,9 @@ const Register = () => {
                 <br />
                 <input type="submit" value="register" />
             </form>
+            <p><small>already have an account <Link to="/login">login</Link></small></p>
             <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
